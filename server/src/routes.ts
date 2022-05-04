b661c36a00558974224eebebb04cfedbeb4f3b6d
Kleverson Kenji <kleversonkenji@gmail.com>
2022-05-04 13:27:05 -0300

    applying solid principles

---
+++ server/src/routes.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
+
+export const routes = express.Router();
+
+var transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "6d1d6518351390",
+    pass: "d9e97c0c7234d1",
+  },
+});
+
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  });
+
+  // await transport.sendMail({
+  //   from: "Equipe Feedget  <oi@feedget.com>",
+  //   to: "Kleverson Iwatani <kleversonkenji@gmail.com>",
+  //   subject: "Novo feedback",
+  //   html: [
+  //     `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //     `</div>`,
+  //   ].join("\n"),
+  // });
+
+  return res.status(201).send();
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,45 +1,10 @@
-import { prisma } from "./prisma";
-import nodemailer from "nodemailer";
 import express from "express";
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-var transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "6d1d6518351390",
-    pass: "d9e97c0c7234d1",
-  },
-});
-
-app.post("/feedbacks", async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
-  });
-
-  await transport.sendMail({
-    from: "Equipe Feedget  <oi@feedget.com>",
-    to: "Kleverson Iwatani <kleversonkenji@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`,
-    ].join("\n"),
-  });
-
-  return res.status(201).json({ data: feedback });
-});
+app.use(routes);
 
 app.listen(4444, () => {
   console.log("HTTP server running!");
