ff31022cd50ce10d6a967f369d78e6ba450f3fba
Kleverson Kenji <kleversonkenji@gmail.com>
2022-05-05 13:47:28 -0300

    react native basic structure implementations

---
+++ mobile/App.tsx
@@ -0,0 +1,38 @@
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+
+import { theme } from "./src/theme";
+import { Widget } from "./src/components/Widget";
+
+export default function App() {
+  // carregar fontes
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  // enquanto fontes não carregarem -> splash screen
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <Widget />
+
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
