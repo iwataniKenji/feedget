663ca1d4803c0a99fdb4f6b8f920f9d2331cf977
Kleverson Kenji <kleversonkenji@gmail.com>
2022-05-05 21:00:49 -0300

    creating form component on react native

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -4,9 +4,14 @@ import { ChatTeardropDots } from "phosphor-react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
+import { Options } from "../Options";
+import { Form } from "../Form";
+
 import { styles } from "./styles";
 import { theme } from "../../theme";
-import { Options } from "../Options";
+import { feedbackTypes } from "../../utils/feedbackTypes";
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -31,7 +36,9 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form 
+          feedbackType='BUG'
+        />
       </BottomSheet>
     </>
   );
