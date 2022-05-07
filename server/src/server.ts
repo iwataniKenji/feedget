6fb1a64dc05f9d809b2f2c45d09773c3bc832bbb
Kleverson Kenji <kleversonkenji@gmail.com>
2022-05-06 23:10:00 -0300

    minor changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.json({ limit: "900kb" }));
+
 app.use(express.json());
 app.use(routes);
 
